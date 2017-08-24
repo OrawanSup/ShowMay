7575397cd9d1cb46736b1ad5c6365bcf294e2a1c
may <may_handball_11@hotmail.com>
2017-08-24 14:47:39 +0700

    Check Gender OK

--- app/src/main/java/khumpong/orawan/showmay/fragment/SignUpFragment.java
+++ app/src/main/java/khumpong/orawan/showmay/fragment/SignUpFragment.java
@@ -1,6 +1,7 @@
 package khumpong.orawan.showmay.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import khumpong.orawan.showmay.MainActivity;
 import khumpong.orawan.showmay.R;
@@ -21,7 +23,8 @@ import khumpong.orawan.showmay.manager.MyAlert;
 public class SignUpFragment extends Fragment{
 
     //Explicit
-    private String nameString, userString, passwordString, rePasswordString;
+    private String nameString, userString, passwordString, rePasswordString, genderString;
+    private boolean genderABoolean = true;  // true ==> Not Choose Gender
 
 
 
@@ -44,7 +47,33 @@ public class SignUpFragment extends Fragment{
         // SignUp Controller
         signUpController();
 
+        //Gender Controller
+        genderController();
+
     }   // onActivityCreate
+
+    private void genderController() {
+        RadioGroup radioGroup = getView().findViewById(R.id.ragGender);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+
+                genderABoolean = false;
+                switch (i) {
+                    case R.id.ragMale:
+                        genderString = "Male";
+                        break;
+                    case R.id.ragFemale:
+                        genderString = "Female";
+                        break;
+                    default:
+                        genderString = "n/a";
+                        break;
+                }   // switch
+
+            }   //onChecked
+        });
+    }
 
     private void signUpController() {
         Button button = getView().findViewById(R.id.btnSignUp);
@@ -77,12 +106,24 @@ public class SignUpFragment extends Fragment{
                     //Password Not Math
                     MyAlert myAlert = new MyAlert(getActivity());
                     myAlert.myDialog("Password Not Math", "Please Fill Same Password");
+                } else if (genderABoolean) {
+                    //Not Choose Gender
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog("Not Choose Gender", "Please Choose Gender");
+
+
+                } else {
+                    uploadValueToServer();
                 }
 
 
 
             }   // onClick
         });
+    }
+
+    private void uploadValueToServer() {
+
     }
 
     private void createToolBar() {
