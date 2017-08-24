23ccc9cff836d596b4d453335f3bcc54f7efa812
may <may_handball_11@hotmail.com>
2017-08-24 13:08:59 +0700

    setUp Title on Toolbar OK

--- app/src/main/java/khumpong/orawan/showmay/fragment/SignUpFragment.java
+++ app/src/main/java/khumpong/orawan/showmay/fragment/SignUpFragment.java
@@ -3,10 +3,12 @@ package khumpong.orawan.showmay.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import khumpong.orawan.showmay.MainActivity;
 import khumpong.orawan.showmay.R;
 
 /**
@@ -17,8 +19,40 @@ public class SignUpFragment extends Fragment{
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
         return view;
+    }   // onCreatview
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //  Create ToolBar
+        createToolBar();
+
+    }   // onActivityCreate
+
+    private void createToolBar() {
+        //SetUp Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolBarSignUp);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle(R.string.register);
+        ((MainActivity)getActivity()).getSupportActionBar().setSubtitle(getString(R.string.sub_register));
+
+
+        //setUp Navagation Icon
+        toolbar.setNavigationIcon(R.mipmap.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getActivity().getSupportFragmentManager()
+                        .popBackStack();
+
+            }   // onClick
+        });
     }
 }   // main Class
