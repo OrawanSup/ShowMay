afb6f7f72350eed16f6b60f7b9a07e542d48c515
may <may_handball_11@hotmail.com>
2017-08-28 11:30:48 +0700

    Read JSON for eBookTABLE OK

--- app/src/main/java/khumpong/orawan/showmay/fragment/ReadEbookFragment.java
+++ app/src/main/java/khumpong/orawan/showmay/fragment/ReadEbookFragment.java
@@ -3,11 +3,15 @@ package khumpong.orawan.showmay.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import khumpong.orawan.showmay.R;
+import khumpong.orawan.showmay.manager.GetAllData;
+import khumpong.orawan.showmay.manager.MyConstant;
 
 /**
  * Created by May on 25/8/2560.
@@ -17,8 +21,38 @@ public class ReadEbookFragment extends Fragment{
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_read_ebook, container, false);
         return view;
+    }   //onCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Create ListView
+        createListView();
+
     }
+
+    private void createListView() {
+        ListView listView = getView().findViewById(R.id.livEbook);
+        String tag = "28AugV1";
+        MyConstant myConstant = new MyConstant();
+
+        try {
+
+            GetAllData getAllData = new GetAllData(getActivity());
+            getAllData.execute(myConstant.getUrlEbookString());
+            String strJSON = getAllData.get();
+            Log.d(tag, "JSON ==>" + strJSON);
+
+        } catch (Exception e) {
+            Log.d(tag, "e createLisView ==>" + e.toString());
+        }
+
+    }   //createLisView
+
 }   // Main Class
---
+++ app/src/main/java/khumpong/orawan/showmay/manager/MyConstant.java
@@ -0,0 +1,14 @@
+package khumpong.orawan.showmay.manager;
+
+/**
+ * Created by May on 28/8/2560.
+ */
+
+public class MyConstant {
+
+    private String urlEbookString = "http://android";
+
+    public String getUrlEbookString() {
+        return urlEbookString;
+    }
+}   //Main Class
